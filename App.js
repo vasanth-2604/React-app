9e11937953362f69ff9d3f896a70e16c2712a14b
Vasanth kumar <vasanth117115@gmail.com>
2025-06-22 22:10:06 +0530

    App ignite

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("div",{id:'parent'},React.createElement("div",{className:'child'},[React.createElement("h1",{id:'heading'},'Hello World'),React.createElement('h2',{},"Hiii")]));
-const root =ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+const heading = React.createElement("div",{id:'parent'},React.createElement("div",{className:'child'},[React.createElement("h1",{id:'heading',key:"H1"},'Hello World'),React.createElement('h2',{className:'SECONDHEADING',key:'H2'},"Hiii")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
