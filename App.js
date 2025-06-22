ff85f1166a2ba184e4829075301558c1f113cec7
vasanth-2604 <vasanth117115@gmail.com>
2025-06-23 01:33:20 +0530

    Revert "Scratched React"

--- App.js
+++ App.js
@@ -2,36 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-//React Element
-const heading =[React.createElement('h1',{key:3},["Hiii from react element",React.createElement('p',{key:1},"heyy from React element")]),React.createElement("h2",{key:6},'header2 from react element')]
 
-const headerRend = ReactDOM.createRoot(document.getElementById('header'));
-const rootRender = ReactDOM.createRoot(document.getElementById('root'));
-
-
-//React JsX syntax
-const headingJsx =(<h1>Hello from 
-    JsX</h1>);
-
-//React Function Component
-const ButtonComponent = ()=> <button id={id} onClick={clicked}>Click me!</button>
- const ParagraphComponent = () => (
-  
-    <div>
-        
-        <p>This is paragraph from react function component</p>
-         <ButtonComponent/> 
-         {heading}
-         {ButtonComponent()}
-    </div>
- )
- 
-
- const id = "btn"
-function clicked(){
-    const button = document.getElementById('btn')
-    button.addEventListener("click",()=>{console.log("button clicked")})
-}
-
-// headerRend.render(heading)
-rootRender.render(<ParagraphComponent/>)
+const heading = React.createElement("div",{id:'parent'},React.createElement("div",{className:'child'},[React.createElement("h1",{id:'heading',key:"H1"},'Hello World'),React.createElement('h2',{className:'SECONDHEADING',key:'H2'},"Hiii")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
